cdbed0c2245378448e529c94086ed91873701afd
BIRSAx2 <Mouhieddinesabir@gmail.com>
2020-11-28 13:44:19 +0100

    Added Recipe and Notes. Established the relationship between them

--- src/main/java/dev/mouhieddine/recipeapp/controllers/IndexController.java
+++ src/main/java/dev/mouhieddine/recipeapp/controllers/IndexController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-  @RequestMapping({"", "/", "index", "index.html"})
+  @RequestMapping({"", "/", "/index", "/index.html"})
   public String getIndexPage(Model model) {
-    return "templates/index";
+    return "index";
   }
 }
