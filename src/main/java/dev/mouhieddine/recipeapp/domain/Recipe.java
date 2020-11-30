8ff1c7e293e51c2bce2c1a8da2dc19027e34e5e3
BIRSAx2 <Mouhieddinesabir@gmail.com>
2020-11-30 21:11:39 +0100

    Refactoring.

--- src/main/java/dev/mouhieddine/recipeapp/domain/Recipe.java
+++ src/main/java/dev/mouhieddine/recipeapp/domain/Recipe.java
@@ -2,6 +2,7 @@ package dev.mouhieddine.recipeapp.domain;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import javax.persistence.*;
 import java.util.HashSet;
@@ -9,6 +10,7 @@ import java.util.Set;
 
 @Data
 @EqualsAndHashCode(exclude= {"ingredients", "notes"})
+@ToString(exclude= {"ingredients", "notes"})
 @Entity
 public class Recipe {
   @Id
